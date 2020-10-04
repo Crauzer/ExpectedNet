cda57014336b8b9e49ceb2ff3d59e11542b3ff5d
Crauzer <filip.quitko@gmail.com>
2020-10-04 20:43:22 +0200

    initial

---
+++ ResultNet/Result.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ResultNet
+{
+    public abstract class Result<T, E>
+    {
+        public static Ok<T> Ok(T value)
+        {
+            return new Ok<T>(value);
+        }
+        public static Error<E> Error(E error)
+        {
+            return new Error<E>(error);
+        }
+
+        public bool IsOk() => this is Ok<T>;
+        public bool IsError() => this is Error<E>;
+
+        public T Unwrap()
+        {
+            switch (this)
+            {
+                case Ok<T> ok: return ok;
+                case Error<T> error: throw new InvalidOperationException("Cannot unwrap an Error Result");
+                default: throw new InvalidOperationException("Unknown Result type");
+            }
+        }
+    }
+
+    public sealed class Ok<T> : Result<T, int>
+    {
+        private T _value;
+
+        public Ok(T value)
+        {
+            this._value = value;
+        }
+
+        public static implicit operator T(Ok<T> result)
+        {
+            return result._value;
+        }
+    }
+
+    public sealed class Error<T> : Result<int, T>
+    {
+        private T _error;
+
+        public Error(T error)
+        {
+            this._error = error;
+        }
+
+        public static implicit operator T(Error<T> result)
+        {
+            return result._error;
+        }
+    }
+}
