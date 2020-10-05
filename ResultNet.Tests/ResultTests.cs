a9d21c084a0a9ae18f55611eb77d352569784f4a
Crauzer <filip.quitko@gmail.com>
2020-10-05 17:00:39 +0200

    add tests #1

---
+++ ResultNet.Tests/ResultTests.cs
@@ -0,0 +1,79 @@
+using NUnit.Framework;
+using System;
+
+namespace ResultNet.Tests
+{
+    [TestFixture(Author = "Crauzer")]
+    public class ResultTests
+    {
+        [Test] public void TestIsOk()
+        {
+            var result = Result<int, string>.Ok(0);
+
+            Assert.IsTrue(result.IsOk());
+        }
+        [Test] public void TestIsError()
+        {
+            var result = Result<int, string>.Error("cats > dogs");
+
+            Assert.IsTrue(result.IsError());
+        }
+
+        [Test] public void TestGetOk()
+        {
+            var result = Result<int, string>.Ok(0);
+            var ok = result.GetOk();
+
+            Assert.IsTrue(ok.HasValue);
+
+        }
+        [Test] public void TestGetError()
+        {
+            var result = Result<int, string>.Error("420");
+            var error = result.GetError();
+
+            Assert.IsTrue(error.HasValue);
+        }
+    
+        [Test] public void TestUnwrap()
+        {
+            var resultOk = Result<int, string>.Ok(0);
+            var resultError = Result<int, string>.Error("kek");
+
+            Assert.DoesNotThrow(() => 
+            {
+                resultOk.Unwrap();
+            });
+
+            Assert.AreEqual(0, resultOk.Unwrap());
+
+            Assert.Throws(typeof(InvalidOperationException), () => 
+            {
+                resultError.Unwrap();
+            });
+        }
+        [Test] public void TestUnwrapOr()
+        {
+            var resultOk = Result<int, string>.Ok(0);
+            var resultError = Result<int, string>.Error("kek");
+
+            Assert.AreEqual(0, resultOk.UnwrapOr(5));
+            Assert.AreEqual(5, resultError.UnwrapOr(5));
+        }
+        [Test] public void TestUnwrapOrElse()
+        {
+            var resultOk = Result<int, string>.Ok(0);
+            var resultError = Result<int, string>.Error("kek");
+
+            Assert.AreEqual(0, resultOk.UnwrapOrElse((string error) =>
+            {
+                return 56;
+            }));
+
+            Assert.AreEqual(56, resultError.UnwrapOrElse((string error) =>
+            {
+                return 56;
+            }));
+        }
+    }
+}
