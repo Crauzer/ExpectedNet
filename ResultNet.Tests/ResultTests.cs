7da039d1d8103c92cd465a26345b1b2cc4ae3871
Crauzer <filip.quitko@gmail.com>
2020-10-05 21:13:20 +0200

    formatting

--- ResultNet.Tests/ResultTests.cs
+++ ResultNet.Tests/ResultTests.cs
@@ -6,20 +6,23 @@ namespace ResultNet.Tests
     [TestFixture(Author = "Crauzer")]
     public class ResultTests
     {
-        [Test] public void TestIsOk()
+        [Test]
+        public void TestIsOk()
         {
             var result = Result<int, string>.Ok(0);
 
             Assert.IsTrue(result.IsOk());
         }
-        [Test] public void TestIsError()
+        [Test]
+        public void TestIsError()
         {
             var result = Result<int, string>.Error("cats > dogs");
 
             Assert.IsTrue(result.IsError());
         }
 
-        [Test] public void TestGetOk()
+        [Test]
+        public void TestGetOk()
         {
             var result = Result<int, string>.Ok(0);
             var ok = result.GetOk();
@@ -27,32 +30,35 @@ namespace ResultNet.Tests
             Assert.IsTrue(ok.HasValue);
 
         }
-        [Test] public void TestGetError()
+        [Test]
+        public void TestGetError()
         {
             var result = Result<int, string>.Error("420");
             var error = result.GetError();
 
             Assert.IsTrue(error.HasValue);
         }
-    
-        [Test] public void TestUnwrap()
+
+        [Test]
+        public void TestUnwrap()
         {
             var resultOk = Result<int, string>.Ok(0);
             var resultError = Result<int, string>.Error("kek");
 
-            Assert.DoesNotThrow(() => 
+            Assert.DoesNotThrow(() =>
             {
                 resultOk.Unwrap();
             });
 
             Assert.AreEqual(0, resultOk.Unwrap());
 
-            Assert.Throws(typeof(InvalidOperationException), () => 
+            Assert.Throws(typeof(InvalidOperationException), () =>
             {
                 resultError.Unwrap();
             });
         }
-        [Test] public void TestUnwrapOr()
+        [Test]
+        public void TestUnwrapOr()
         {
             var resultOk = Result<int, string>.Ok(0);
             var resultError = Result<int, string>.Error("kek");
@@ -60,7 +66,8 @@ namespace ResultNet.Tests
             Assert.AreEqual(0, resultOk.UnwrapOr(5));
             Assert.AreEqual(5, resultError.UnwrapOr(5));
         }
-        [Test] public void TestUnwrapOrElse()
+        [Test]
+        public void TestUnwrapOrElse()
         {
             var resultOk = Result<int, string>.Ok(0);
             var resultError = Result<int, string>.Error("kek");
@@ -76,7 +83,8 @@ namespace ResultNet.Tests
             }));
         }
 
-        [Test] public void TestMap()
+        [Test]
+        public void TestMap()
         {
             var resultOk = Result<int, string>.Ok(0);
             var resultError = Result<int, string>.Error("kek");
@@ -84,7 +92,8 @@ namespace ResultNet.Tests
             Assert.AreEqual(1, resultOk.Map((int x) => { return x + 1; }).Unwrap());
             Assert.IsTrue(resultError.Map((int x) => { return x + 1; }).IsError());
         }
-        [Test] public void TestMapOr()
+        [Test]
+        public void TestMapOr()
         {
             var resultOk = Result<int, string>.Ok(0);
             var resultError = Result<int, string>.Error("kek");
@@ -92,7 +101,8 @@ namespace ResultNet.Tests
             Assert.AreEqual(5, resultOk.MapOr(20, (int x) => { return x + 5; }));
             Assert.AreEqual(20, resultError.MapOr(20, (int x) => { return x + 5; }));
         }
-        [Test] public void TestMapOrElse()
+        [Test]
+        public void TestMapOrElse()
         {
             var resultOk = Result<int, string>.Ok(0);
             var resultError = Result<int, string>.Error("kek");
@@ -101,7 +111,8 @@ namespace ResultNet.Tests
             Assert.AreEqual(1, resultError.MapOrElse((string error) => { return 1; }, (int x) => { return 2; }));
         }
 
-        [Test] public void TestAnd()
+        [Test]
+        public void TestAnd()
         {
             var resultOk = Result<int, string>.Ok(0);
             var resultError = Result<int, string>.Error("kek");
@@ -109,7 +120,8 @@ namespace ResultNet.Tests
             Assert.AreEqual(5, resultOk.And(Result<int, string>.Ok(5)).Unwrap());
             Assert.IsTrue(resultError.And(Result<int, string>.Ok(5)).IsError());
         }
-        [Test] public void TestAndThen()
+        [Test]
+        public void TestAndThen()
         {
             var resultOk = Result<int, string>.Ok(0);
             var resultError = Result<int, string>.Error("kek");
@@ -125,7 +137,8 @@ namespace ResultNet.Tests
             }).IsError());
         }
 
-        [Test] public void TestOr()
+        [Test]
+        public void TestOr()
         {
             var resultOk = Result<int, string>.Ok(0);
             var resultError = Result<int, string>.Error("kek");
@@ -133,7 +146,8 @@ namespace ResultNet.Tests
             Assert.AreEqual(0, resultOk.Or(Result<int, string>.Ok(5)).Unwrap());
             Assert.AreEqual(5, resultError.Or(Result<int, string>.Ok(5)).Unwrap());
         }
-        [Test] public void TestOrElse()
+        [Test]
+        public void TestOrElse()
         {
             var resultOk = Result<int, string>.Ok(0);
             var resultError = Result<int, string>.Error("kek");
